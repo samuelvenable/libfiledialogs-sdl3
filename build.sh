68d74ed75c383f6e46a1ec03096faf59a54027f0
high on tantor <josephvenable@verizon.net>
2025-06-01 14:21:22 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,18 +7,6 @@ if [ `uname` = "Darwin" ]; then
   else
     echo "MacPorts installation not found! Please download and install MacPorts first from www.macports.org";
   fi
-elif [ $(uname) = "Linux" ]; then
-  "./filedialogs/build.sh";
-elif [ $(uname) = "FreeBSD" ]; then
-  "./filedialogs/build.sh";
-elif [ $(uname) = "DragonFly" ]; then
-  "./filedialogs/build.sh";
-elif [ $(uname) = "NetBSD" ]; then
-  "./filedialogs/build.sh";
-elif [ $(uname) = "OpenBSD" ]; then
-  "./filedialogs/build.sh";
-elif [ $(uname) = "SunOS" ]; then
-  "./filedialogs/build.sh";
-else # Windows
+else
   "./filedialogs/build.sh";
 fi
