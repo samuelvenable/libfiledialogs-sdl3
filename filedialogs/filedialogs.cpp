a4eabb641d852c4ecace61ed997931c184818913
high on tantor <josephvenable@verizon.net>
2025-06-05 16:17:48 -0400

    Update filedialogs.cpp

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -604,7 +604,6 @@ namespace {
         bool inside = false;
         int x = 0, y = 0, w = 0, h = 0;
         SDL_GetWindowPosition(window, &x, &y);
-        printf("1\n");
         if (SDL_GetCurrentRenderOutputSize(SDL_GetRenderer(window), &w, &h)) {
           int numDisplays = 0;
           SDL_DisplayID *dispId = SDL_GetDisplays(&numDisplays);
