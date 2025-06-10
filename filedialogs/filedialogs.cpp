9b2143ab96d8bc6a2d39a8452cae4021151a819a
high on tantor <josephvenable@verizon.net>
2025-06-10 00:47:48 -0400

    Update filedialogs.cpp

--- filedialogs/filedialogs.cpp
+++ filedialogs/filedialogs.cpp
@@ -38,8 +38,8 @@
 
 #include <imgui/imgui.h>
 
-#include <imgui/backends/imgui_impl_sdl2.h>
-#include <imgui/backends/imgui_impl_sdlrenderer2.h>
+#include <imgui/backends/imgui_impl_sdl3.h>
+#include <imgui/backends/imgui_impl_sdlrenderer3.h>
 
 #include <ImFileDialogs/ImFileDialog.h>
 #include <ImFileDialogs/ImFileDialogMacros.h>
