536166cc15391f12805656b456585604cda49cfb
high on tantor <josephvenable@verizon.net>
2025-07-05 23:07:06 -0400

    Update build.sh

--- filedialogs/build.sh
+++ filedialogs/build.sh
@@ -3,7 +3,7 @@ cd "${0%/*}"
 
 # build command line executable
 if [ `uname` = "Darwin" ]; then
-  sudo port install SDL3 +universal;
+  sudo port install SDL3 +universal libiconv +universal;
   clang++ "ImFileDialog/ImFileDialog.cpp" "imgui/imgui.cpp" "imgui/imgui_impl_sdl3.cpp" "imgui/imgui_impl_sdlgpu3.cpp" "imgui/imgui_impl_sdlrenderer3.cpp" "imgui/imgui_draw.cpp" "imgui/imgui_tables.cpp" "imgui/imgui_widgets.cpp" "filesystem.cpp" "filedialogs.cpp" "msgbox/imguial_msgbox.cpp" "main.cpp" -o "filedialogs" -std=c++17 -Wno-format-security -I. -DIMGUI_USE_WCHAR32 -I/opt/local/include -I/opt/local/include -ObjC++ `pkg-config --cflags --libs sdl3 --static` -liconv -Wl,-framework,CoreAudio -Wl,-framework,AudioToolbox -Wl,-weak_framework,CoreHaptics -Wl,-weak_framework,GameController -Wl,-framework,ForceFeedback -lobjc -Wl,-framework,CoreVideo -Wl,-framework,Cocoa -Wl,-framework,Carbon -Wl,-framework,IOKit -Wl,-weak_framework,QuartzCore -Wl,-weak_framework,Metal -fPIC -arch arm64 -arch x86_64 -fPIC;
   cp -fr "/opt/local/lib/libSDL3.dylib" "./libSDL3.dylib"
   install_name_tool -id @loader_path/libSDL3.dylib ./libSDL3.dylib
