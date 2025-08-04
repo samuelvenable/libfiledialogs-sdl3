9d008e5bb4ae6f8550980d6294cfbe7c69a04a00
high on tantor <josephvenable@verizon.net>
2025-08-04 04:01:00 -0400

    Update build.sh

--- filedialogs/build.sh
+++ filedialogs/build.sh
@@ -20,6 +20,15 @@ elif [ $(uname) = "Linux" ]; then
   cmake .;
   make;
   cd ..;
+  git clone -b release-3.2.x https://github.com/libsdl-org/SDL;
+  cd SDL;
+  mkdir build;
+  cd build;
+  cmake ..;
+  make;
+  sudo make install;
+  ln -s "/usr/local/lib/libSDL3.so" "./libSDL3.so"
+  ln -s "/usr/local/lib/libSDL3.so.0" "./libSDL3.so.0"
   g++ "ImFileDialog/ImFileDialog.cpp" "imgui/imgui.cpp" "imgui/imgui_impl_sdl3.cpp" "imgui/imgui_impl_sdlgpu3.cpp" "imgui/imgui_impl_sdlrenderer3.cpp" "imgui/imgui_draw.cpp" "imgui/imgui_tables.cpp" "imgui/imgui_widgets.cpp" "filesystem.cpp" "filedialogs.cpp" "msgbox/imguial_msgbox.cpp" "main.cpp" -o "filedialogs" -std=c++17 -Wno-format-security -I. -Ilunasvg/include "lunasvg/liblunasvg.a" "lunasvg/plutovg/libplutovg.a" -DIMGUI_USE_WCHAR32 -static-libgcc -static-libstdc++ `pkg-config --cflags sdl3 --static` `pkg-config --cflags --libs gtk+-3.0` `pkg-config --cflags --libs gio-2.0` `pkg-config --cflags --libs glib-2.0` -lX11 -lpthread -no-pie -Wl,--rpath="\$ORIGIN" -lSDL3 -fPIC;
 elif [ $(uname) = "FreeBSD" ]; then
   cd "lunasvg";
