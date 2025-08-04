fe61e28dbfe731d6d615e88848a38aee9768f69a
high on tantor <josephvenable@verizon.net>
2025-08-03 22:02:09 -0400

    Update ImFileDialog.cpp

--- filedialogs/ImFileDialog/ImFileDialog.cpp
+++ filedialogs/ImFileDialog/ImFileDialog.cpp
@@ -796,9 +796,9 @@ namespace ifd {
     std::error_code ec;
     auto exists = [](ghc::filesystem::path path) {
       #if defined(_WIN32)
-      return (!path.wstring().empty() && path.is_absolute() && 
-      INVALID_FILE_ATTRIBUTES != GetFileAttributesW(path.wstring().c_str()) && 
-      GetLastError() != ERROR_FILE_NOT_FOUND);
+      return (!path.wstring().empty() && path.is_absolute() && path.wstring().length() >= 2 &&
+      (!(path.wstring()[0] == '\\' && path.wstring()[1] != '\\')) && INVALID_FILE_ATTRIBUTES != 
+      GetFileAttributesW(path.wstring().c_str()) && GetLastError() != ERROR_FILE_NOT_FOUND);
       #else
       std::error_code ec;
       return (!path.u8string().empty() && path.is_absolute() && 
